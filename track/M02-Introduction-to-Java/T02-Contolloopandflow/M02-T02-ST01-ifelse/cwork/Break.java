280556e5f31f98b64d307d14b8f9ad413beac50f
charan2477 <charan24teja@gmail.com>
2026-08-06 12:38:38 +0530

    day4-2

---
+++ track/M02-Introduction-to-Java/T02-Contolloopandflow/M02-T02-ST01-ifelse/cwork/Break.java
@@ -0,0 +1,10 @@
+public class Break {
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            System.err.println(i);
+            if(i==6){
+                break;
+            }
+        }
+    }
+}
